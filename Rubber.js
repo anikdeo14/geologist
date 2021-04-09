e5a43c2e081dd1c0de1458cda2c80c8c968f7128
anikdeo14 <80943708+anikdeo14@users.noreply.github.com>
2021-04-09 23:51:44 +0530

    Update Rubber.js

--- Rubber.js
+++ Rubber.js
@@ -1,19 +1,17 @@
-class Rubber{
+class Rubber{ 
 	constructor(x,y,r)
-	{
-	// assign options to the rubber ball
-		this.x=x;
-		this.y=y;
-		this.r=r
-		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
-		World.add(world, this.body);
-		var options= {
-			'restitution': 0.3,
-			'friction': 5,
-			'density': 1
-		}
+	{ var options={
+		restitution:0.3,
+		friction:5, 
+		density:1
+	} 
+	this.x=x; 
+	 this.y=y;
+	 this.r=r
+	 this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+	 World.add(world, this.body); }
 
-	}
+}
 	display()
 	{
 			var rubberpos=this.body.position;		
